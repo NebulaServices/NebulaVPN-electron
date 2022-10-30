8547f7e908eedeeb8a26a444d25f3386d73f6c16
Linux Snoot <49733954+NovaAppsInc@users.noreply.github.com>
2022-10-29 20:05:06 -0500

    minor fix

--- source/main.js
+++ source/main.js
@@ -2,6 +2,7 @@ const connected = document.querySelector('.connected');
 const serverConnect = document.querySelectorAll('.serverConnect');
 const settingsIcon = document.querySelector('.settings');
 const settingsPanel = document.querySelector('.settingsPanelC');
+const settingsClose = document.querySelector('.settingsClose');
 
 connected.addEventListener("mouseover", function() {
     connected.innerHTML = "DISCONNECT";
@@ -11,5 +12,13 @@ connected.addEventListener("mouseover", function() {
 });
 
 settingsIcon.addEventListener("click", (e) => {
-    settingsPanel.classList.toggle("show");
-})
+    if(!settingsPanel.classList.contains("show")) {
+        settingsPanel.classList.toggle("show");
+    }
+});
+
+settingsClose.addEventListener("click", (e) => {
+    if(settingsPanel.classList.contains("show")) {
+        settingsPanel.classList.toggle("show");
+    }
+});
